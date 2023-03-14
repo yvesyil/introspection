cf7b88dcf8aff77edaf6dadd93c5ae3df4f90831
yvesyil <doganyigityilmazz@gmail.com>
2023-03-14 03:50:23 +0000

    added editors, custom fonts, and github dark theme

--- client/introspection/src/App.tsx
+++ client/introspection/src/App.tsx
@@ -8,35 +8,61 @@ import {
   DialogContent,
   Button,
 } from "@fluentui/react-components";
-import './App.css'
+import {editor} from 'monaco-editor';
+import Editor, { useMonaco } from "@monaco-editor/react";
+import { makeStyles } from '@fluentui/react-components';
+
+import './App.css';
+import GithubDarkTheme from './assets/github-dark.json';
+import { useEffect } from "react";
+
+const useStyles = makeStyles({
+  editor_container: {
+    display: 'flex',
+    height: '100vh'
+  },
+});
+
 
 function App() {
+  const classes = useStyles();
+  const editorFont = "'Source Code Pro', Menlo, Monaco, Consolas, 'Courier New', monospace";
+
+  const defaultCode = `// paste your code here\n#include <stdio.h>\n\nint main(void)\n{\n\tprintf("Hello, World!");\n\treturn 0;\n}\n`;
+
+  const monaco = useMonaco();
+
+  useEffect(() => {
+    if (monaco) {
+      monaco.editor.defineTheme('github-dark', GithubDarkTheme as editor.IStandaloneThemeData);
+      monaco.editor.setTheme('github-dark');
+    }
+  }, [monaco]);
+
   return (
     <div id="container">
-      <Dialog>
-        <DialogTrigger disableButtonEnhancement>
-          <Button>Open dialog</Button>
-        </DialogTrigger>
-        <DialogSurface>
-          <DialogBody>
-            <DialogTitle>Dialog title</DialogTitle>
-            <DialogContent>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              exercitationem cumque repellendus eaque est dolor eius expedita
-              nulla ullam? Tenetur reprehenderit aut voluptatum impedit voluptates
-              in natus iure cumque eaque?
-            </DialogContent>
-            <DialogActions>
-              <DialogTrigger disableButtonEnhancement>
-                <Button appearance="secondary">Close</Button>
-              </DialogTrigger>
-              <Button appearance="primary">Do Something</Button>
-            </DialogActions>
-          </DialogBody>
-        </DialogSurface>
-      </Dialog>
+      <Button shape="square">Compile</Button>
+      <div className={classes.editor_container}>
+        <Editor
+          width="50vw"
+          defaultLanguage="c"
+          defaultValue={defaultCode}
+          theme='vs-dark'
+          options={{
+            fontFamily: editorFont
+          }}
+        />
+        <Editor
+          width="50vw"
+          defaultLanguage="plaintext"
+          theme="vs-dark"
+          options={{
+            fontFamily: editorFont
+          }}
+        />
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
--- client/introspection/src/main.tsx
+++ client/introspection/src/main.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { FluentProvider, teamsDarkTheme } from '@fluentui/react-components'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { FluentProvider, teamsDarkTheme, Theme } from '@fluentui/react-components';
+import App from './App';
 
-import App from './App'
+import './index.css';
 
-import './index.css'
+const theme: Theme = {
+  ...teamsDarkTheme,
+  fontFamilyBase: "'Nunito Sans', 'Segoe UI', sans-serif",
+  fontWeightRegular: 600,
+  fontWeightBold: 900
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <FluentProvider theme={teamsDarkTheme}>
+  <FluentProvider theme={theme}>
     <App />
   </FluentProvider>
-)
+);
