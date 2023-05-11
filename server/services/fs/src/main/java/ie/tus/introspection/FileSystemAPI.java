bf8e2253a93d8040a7d469efddbb396cdb1728d5
yvesyil <doganyigityilmazz@gmail.com>
2023-05-11 17:03:22 +0100

    fs rewrite

--- server/services/fs/src/main/java/ie/tus/introspection/FileSystemAPI.java
+++
@@ -1,9 +0,0 @@
-package ie.tus.introspection;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class FileSystemAPI extends Application {
-
-}
